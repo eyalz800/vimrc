d9da5314bf03b31687ecc01ad0b7d4bec22ac06d
eyalz800 <eyalz800@gmail.com>
2020-09-19 17:19:48 +0300

    Compiling vim from source to allow clipboard.

--- install.sh
+++ install.sh
@@ -4,38 +4,48 @@ set -e
 if [ -x "$(command -v brew)" ]; then
     sudo -u $SUDO_USER brew install vim || true
 else
-    if ! [ -x "$(command -v python3)" ]; then
+    vim_version=8.2.1539
+    python3_command=python3
+
+    if ! [ -x "$(command -v $python3_command)" ]; then
         DEBIAN_FRONTEND=noninteractive apt install -y python3
     fi
 
-    if ! [ $(python3 --version | python3 -c "import sys; major, minor = [int(c) for c in sys.stdin.read().split(\" \")[1].split(\".\")][:2]; print(1 if major >= 3 and minor >= 6 else 0)") == 1 ]; then
+    if ! [ $(python3 -c "import sys; print(1 if sys.version_info.major >= 3 and sys.version_info.minor >= 6 else 0)") == 1 ]; then
         if ! [ -x "$(command -v python3.6)" ]; then
             add-apt-repository -y ppa:deadsnakes/ppa
             apt update
             DEBIAN_FRONTEND=noninteractive apt install -y python3.6-dev
         fi
-        DEBIAN_FRONTEND=noninteractive apt install -y curl build-essential make libncurses5-dev libncursesw5-dev gcc
-        curl -fLo ~/.vim/tmp/vim/vim.tar.gz --create-dirs \
-            https://github.com/vim/vim/archive/v8.2.1539.tar.gz
-        INSTALL_VIMRC_CURDIR=`pwd`
-        cd ~/.vim/tmp/vim
-        tar -xzf ~/.vim/tmp/vim/vim.tar.gz
-        cd vim-8.2.1539
-        ./configure --with-features=huge \
-            --enable-fail-if-missing \
-            --prefix=/usr \
-            --enable-cscope \
-            --enable-python3interp \
-            --with-python3-command=python3.6 \
-            --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu
-        make -j
-        make install
-        cd $INSTALL_VIMRC_CURDIR
-    else
-        add-apt-repository -y ppa:jonathonf/vim
-        apt update
-        apt install -y vim
+        python3_command=python3.6
     fi
+
+    DEBIAN_FRONTEND=noninteractive apt install -y \
+        curl build-essential make libncurses5-dev libncursesw5-dev gcc \
+        libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
+    DEBIAN_FRONTEND=noninteractive apt purge -y vim
+
+    if ! [ -d ~/.vim/tmp/vim/vim-$vim-version ]
+        curl -fLo ~/.vim/tmp/vim/vim.tar.gz --create-dirs \
+            https://github.com/vim/vim/archive/v$vim_version.tar.gz
+    fi
+
+    INSTALL_VIMRC_CURDIR=`pwd`
+    cd ~/.vim/tmp/vim
+    tar -xzf ~/.vim/tmp/vim/vim.tar.gz
+    cd vim-$vim_version
+    make distclean
+    ./configure --with-features=huge \
+        --enable-fail-if-missing \
+        --prefix=/usr \
+        --enable-cscope \
+        --enable-python3interp \
+        --enable-clipboard \
+        --enable-xterm_clipboard \
+        --with-python3-command=$python3_command
+    make -j
+    make install
+    cd $INSTALL_VIMRC_CURDIR
 fi
 cp .vimrc ~/.vimrc
 INSTALL_VIMRC=1 vim
