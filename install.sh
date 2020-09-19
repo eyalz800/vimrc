1358425a6189da44b4b7380d4ab11bcaea6c9d58
eyalz800 <eyalz800@gmail.com>
2020-09-19 17:33:05 +0300

    Making sure appropriate python-dev is installed for vim compilation.

--- install.sh
+++ install.sh
@@ -7,23 +7,19 @@ else
     vim_version=8.2.1539
     python3_command=python3
 
-    if ! [ -x "$(command -v $python3_command)" ]; then
-        DEBIAN_FRONTEND=noninteractive apt install -y python3
-    fi
+    DEBIAN_FRONTEND=noninteractive apt install -y \
+        python3-dev curl build-essential make libncurses5-dev libncursesw5-dev gcc \
+        libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
     if ! [ $(python3 -c "import sys; print(1 if sys.version_info.major >= 3 and sys.version_info.minor >= 6 else 0)") == 1 ]; then
-        if ! [ -x "$(command -v python3.6)" ]; then
-            add-apt-repository -y ppa:deadsnakes/ppa
-            apt update
+        if ! [ -x "$(command -v python3.6-dev)" ] || \
+             [ $(dpkg-query -W -f='${Status}' python3.6-dev 2>/dev/null | grep -c "ok installed") == 1 ]; then
+            DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:deadsnakes/ppa
+            DEBIAN_FRONEND=noninteractive apt update
             DEBIAN_FRONTEND=noninteractive apt install -y python3.6-dev
         fi
         python3_command=python3.6
     fi
-
-    DEBIAN_FRONTEND=noninteractive apt install -y \
-        curl build-essential make libncurses5-dev libncursesw5-dev gcc \
-        libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
-    DEBIAN_FRONTEND=noninteractive apt purge -y vim
 
     if ! [ -d ~/.vim/tmp/vim/vim-$vim-version ]
         curl -fLo ~/.vim/tmp/vim/vim.tar.gz --create-dirs \
@@ -44,6 +40,9 @@ else
         --enable-xterm_clipboard \
         --with-python3-command=$python3_command
     make -j
+
+    DEBIAN_FRONTEND=noninteractive apt purge -y vim
+
     make install
     cd $INSTALL_VIMRC_CURDIR
 fi
