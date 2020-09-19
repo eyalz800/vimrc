d0a28bf75869b4ed1a227d16893cd0ffab08bf23
eyalz800 <eyalz800@gmail.com>
2020-09-19 17:43:46 +0300

    Corrected way of checking for python3.6.

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ else
         libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
     if ! [ $(python3 -c "import sys; print(1 if sys.version_info.major >= 3 and sys.version_info.minor >= 6 else 0)") == 1 ]; then
-        if ! [ -x "$(command -v python3.6-dev)" ] || \
-             [ $(dpkg-query -W -f='${Status}' python3.6-dev 2>/dev/null | grep -c "ok installed") == 1 ]; then
+        if ! [ -x "$(command -v python3.6)" ] || \
+           ! [ $(dpkg-query -W -f='${Status}' python3.6-dev 2>/dev/null | grep -c "ok installed") == 1 ]; then
             DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:deadsnakes/ppa
             DEBIAN_FRONEND=noninteractive apt update
             DEBIAN_FRONTEND=noninteractive apt install -y python3.6-dev
