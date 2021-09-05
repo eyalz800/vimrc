ea38535452a45766e55f5506f2ad8e448e4f9a9f
eyalz800 <eyalz800@gmail.com>
2021-09-05 21:24:01 +0300

    Updated vim version.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 if [ -x "$(command -v brew)" ]; then
     sudo -u $SUDO_USER brew install vim || true
 else
-    vim_version=8.2.2847
+    vim_version=8.2.3404
     python3_command=python3
 
     DEBIAN_FRONTEND=noninteractive apt update
